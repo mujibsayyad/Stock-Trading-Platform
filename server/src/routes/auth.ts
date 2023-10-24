d48f6fa84437b60aaf63016bb7dbf3957f86ab7b
mujibsayyad <mujibsayyad97@gmail.com>
2023-10-24 23:07:39 +0530

    added google login route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -8,10 +8,13 @@ import {
   redirectUpstox,
 } from '../controllers/auth';
 import isAuthenticate from '../middlewares/isAuth';
+import { googleOAuthHandler } from '../googleAuth/googleOAuthHandler';
 
 const router = Router();
 
 router.get('/validate', isAuthenticate, validateLogin);
+
+router.get('/oauth/google', googleOAuthHandler);
 
 router.get('/upstox', loginUpstox);
 
