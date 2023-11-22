ec08dea168dc1e4ac2675e0b512fff894448bc31
mujibsayyad <mujibsayyad97@gmail.com>
2023-11-22 23:55:43 +0530

    updated next.config

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp4|webm)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next',
+          name: 'static/media/[name].[hash].[ext]',
+        },
+      },
+    });
+
+    return config;
+  },
+};
 
 module.exports = nextConfig;
