973451ad202c662c7db77096fc358ada4d6343c5
mujibsayyad <mujibsayyad97@gmail.com>
2023-11-28 23:33:19 +0530

    metadata updated

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -11,6 +11,19 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Stock Trading Platform',
   description: 'Real Time Stock Trading Platform',
+  openGraph: {
+    authors: 'Mujib Sayyad',
+    title: 'Stock Trading Platform',
+    description: 'Indian NSE Stock Trading Platform',
+    url: 'https://stock-platform.vercel.app',
+    images: [
+      {
+        url: 'https://res.cloudinary.com/dcvbqthie/image/upload/v1701194279/STP_meta.png',
+        width: 600,
+        height: 600,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
